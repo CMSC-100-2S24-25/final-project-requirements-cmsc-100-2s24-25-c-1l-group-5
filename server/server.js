3e4ade610fadadcd52acd355e7b6ebc97001bc8d
hfroldan <hfroldan@up.edu.ph>
2025-04-25 20:50:09 +0800

    feat: integrate MongoDB Atlas for cloud-based database management

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // setup the server and added parser to read incoming requests
 const app = express();
+dotenv.config();
 
 // Below acts as a plugin in order to read JSON contents of incoming request messages (Middleware)
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to Database
-// connectDB();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}${process.env.DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
+
+connectDB();
 
 // Routes 
 // app.use('/api/products', productRoutes); (placeholder for later)
